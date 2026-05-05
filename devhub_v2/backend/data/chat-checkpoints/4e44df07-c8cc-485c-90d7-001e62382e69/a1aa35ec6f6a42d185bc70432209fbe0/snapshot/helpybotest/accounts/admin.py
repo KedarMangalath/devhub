e2bf55bc4b4e456f84cb8b4ff94a8acb7b547063
Kedar Mangalath <mangalathkedar@gmail.com>
2026-05-05 09:45:13 +0530

    Cleanup: remove runtime data and ignore large files

--- devhub_v2/backend/data/chat-checkpoints/4e44df07-c8cc-485c-90d7-001e62382e69/a1aa35ec6f6a42d185bc70432209fbe0/snapshot/helpybotest/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Chatbot, PDFStore, ChatHistory
-
-admin.site.register(User)
-admin.site.register(Chatbot)
-admin.site.register(PDFStore)
-admin.site.register(ChatHistory)
--- devhub_v2/backend/data/chat-checkpoints/beb869ba-1a41-4a39-9a1a-8d3d931d3ae5/f4fa4131b9e24d1cb7cca08a8163fd2a/snapshot/README.md
+++
@@ -1,2 +0,0 @@
-# Trace Demo
-Use the App component.
